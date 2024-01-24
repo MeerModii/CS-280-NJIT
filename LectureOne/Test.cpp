dc3ef0d942c52ee3fe5581fa7a67a3ac66408d89
Meer Modi <116285279+MeerModii@users.noreply.github.com>
2024-01-24 14:47:50 -0500

    Assignment 1

--- LectureOne/Test.cpp
+++ LectureOne/Test.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
 using namespace std;
 
-int main(int argc, char *argv[]) {
-    std::cout << "Hello World!";
-    return 0;
+int main() {
+    string first_name;
+    string last_name;
+    int section_number;
+    cout << "Please enter your first name >>" << endl;
+    cin >> first_name;
+    cout << "Please enter your last name >>"<< endl;
+    cin >> last_name;
+    cout << "Please enter your CS 280 section number >>"<< endl;
+    cin >> section_number;
+    cout << "Hello " << first_name << ", welcome to CS 280 section " << section_number << endl;
 } 
